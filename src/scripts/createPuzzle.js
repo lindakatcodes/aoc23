e986804463e4ac0939c05155b9d7767988fcb3df
LindaKat <lindakt16@gmail.com>
2023-11-30 03:10:19 -0600

    init setup, creation of files script created

---
+++ src/scripts/createPuzzle.js
@@ -0,0 +1,31 @@
+const prompt = require("prompt-sync")({ sigint: true });
+const { writeFile } = require("fs/promises");
+
+const day = Number(prompt("What day is it: "));
+const needsInput = Boolean(prompt("Do you need an input file? "));
+
+//  make the file name with the day
+const paddedDay = day < 10 ? `0${day}` : day;
+const dayFile = `./src/2023/puzzles/day${paddedDay}.js`;
+// make an input file if needed
+const inputFile = `./src/2023/inputs/day${paddedDay}Input.txt`;
+// store the string I want to be written by default to the file
+const template = `
+  import * as h from "../helpers";
+  ${needsInput && `const initData = await h.readData("${inputFile}");`}
+`;
+// write the string to the file
+(async () => {
+  try {
+    await writeFile(`${dayFile}`, template);
+    if (needsInput) {
+      await writeFile(inputFile, "");
+    }
+    // final console log and close out
+    console.log(`Files for day ${paddedDay} are created! You got this!`);
+  } catch (error) {
+    console.error(
+      `Got an error trying to write the file: ${error.message}, ${template}`
+    );
+  }
+})();
